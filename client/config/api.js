e63b4d863c67da114efe67bcf0baa373d25f8b89
Neo <fullstackneo@gmail.com>
2022-07-05 23:55:52 -0600

    replace file

--- client/config/api.js
+++
@@ -1,6 +0,0 @@
-export let API;
-if (process.env.NODE_ENV === 'production') {
-  API = process.env.REACT_APP_PRODUCTION_API_URL;
-} else if (process.eng.NODE_ENV === 'development') {
-  API = process.env.REACT_APP_DEVLOPMENT_API_URL;
-}
