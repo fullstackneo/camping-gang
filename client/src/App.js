1aa6fe6bd496cf8f39e3b281273f556d0a0f3102
Neo <fullstackneo@gmail.com>
2022-07-07 22:05:24 -0600

    adjust router structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Admin from './pages/Admin';
 import SiteList from './pages/SiteList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Row, Col } from 'antd';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import NavLeft from './components/layout/NavLeft';
 
 // import logo from './logo.svg';
 
 function App() {
   return (
-    <>
-      <Admin />
-      {/* <SiteList /> */}
-    </>
+    <Router>
+      <Row>
+        <Col span={3}>
+          <NavLeft />
+        </Col>
+        <Col span={21}>
+          <Header />
+          <Row>
+            <Routes>
+              <Route path="/" element={<Admin />} />
+              <Route path="/campsites" element={<SiteList />} />
+            </Routes>
+          </Row>
+          <Footer />
+        </Col>
+      </Row>
+    </Router>
   );
 }
 
--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,23 +1,6 @@
-import { Row, Col } from 'antd';
-import Footer from '../../components/layout/Footer';
-import Header from '../../components/layout/Header';
 import Home from '../../components/layout/Home';
-import NavLeft from '../../components/layout/NavLeft';
 
 const Admin = () => {
-  return (
-    <Row>
-      <Col span={3}>
-        <NavLeft />
-      </Col>
-      <Col span={21}>
-        <Header />
-        <Row>
-          <Home />
-        </Row>
-        <Footer />
-      </Col>
-    </Row>
-  );
+  return <Home />;
 };
 export default Admin;
